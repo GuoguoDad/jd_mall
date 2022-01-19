23512a698ab9e097a22f96dc6ea991518f58711f
刘辉20020013 <20020013@suning.com>
2022-01-19 10:31:40 +0800

    feat: 细节调整

--- app/src/main/java/com/example/news/ui/waterfall/WaterfallListAdapter.kt
+++ app/src/main/java/com/example/news/ui/waterfall/WaterfallListAdapter.kt
@@ -6,6 +6,8 @@ import com.example.news.R
 
 open class WaterfallListAdapter(layoutResId: Int, data: MutableList<ProductBean>): BaseQuickAdapter<ProductBean, BaseViewHolder>(layoutResId, data) {
     override fun convert(holder: BaseViewHolder, item: ProductBean) {
-        holder.setImageResource(R.id.waterfall_item_img, item.img).setText(R.id.waterfall_item_title, item.title)
+        holder
+            .setImageResource(R.id.waterfall_item_img, item.img).
+            setText(R.id.waterfall_item_title, item.title)
     }
 }
