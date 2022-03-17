46bab9c3ce5f6d2e0f0199996b3130f5a5d51c78
刘辉20020013 <20020013@suning.com>
2022-03-17 12:58:01 +0800

    chore: 分类页性能优化

--- module_category/src/main/java/com/aries/category/ui/CategoryFragment.kt
+++ module_category/src/main/java/com/aries/category/ui/CategoryFragment.kt
@@ -1,6 +1,7 @@
 package com.aries.category.ui
 
 import android.annotation.SuppressLint
+import android.graphics.Rect
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.airbnb.mvrx.MavericksView
 import com.airbnb.mvrx.activityViewModel
@@ -17,11 +18,18 @@ class CategoryFragment : BaseFragment(R.layout.fragment_main), MavericksView {
     private val categoryListAdapter by lazy { CategoryListAdapter(R.layout.main_left_item, arrayListOf())  }
     private val categoryLayoutManager: LinearLayoutManager by lazy { LinearLayoutManager(this.activity) }
 
+    private var visualHeight = -1 //recyclerView 可视区域高度 - 当前点击item的高度
+
     override fun initView() {
         categoryList.run {
+            setHasFixedSize(true)
             layoutManager = categoryLayoutManager
             adapter = categoryListAdapter
-            categoryListAdapter.setOnItemClickListener{ _, _, position ->
+            categoryListAdapter.setOnItemClickListener{ _, item, position ->
+                var rect = Rect()
+                categoryList.getGlobalVisibleRect(rect)
+                visualHeight = rect.bottom - rect.top - item.height
+
                 setSelectCategory(position)
                 scrollToMiddle(position)
                 rightViewModel.queryContentByCate(categoryListAdapter.data[position].code)
@@ -65,27 +73,35 @@ class CategoryFragment : BaseFragment(R.layout.fragment_main), MavericksView {
     private fun setSelectCategory(selectIndex: Int) {
         categoryListAdapter.run {
             var preSelectIndex = data.indexOfFirst { v -> v.isSelect == true }
+            data[preSelectIndex].isSelect = false
             if (preSelectIndex - 1 >= 0) {
                 data[preSelectIndex - 1].fillet = ""
+                notifyItemRangeChanged(preSelectIndex - 1, 2)
             }
             if (preSelectIndex + 1 < categoryListAdapter.data.size) {
                 data[preSelectIndex + 1].fillet = ""
+                notifyItemRangeChanged(preSelectIndex, 2)
             }
 
-            data[preSelectIndex].isSelect = false
             data[selectIndex].isSelect = true
             if (selectIndex - 1 >= 0) {
                 data[selectIndex - 1].fillet = "down"
+                notifyItemRangeChanged(selectIndex - 1, 2)
             }
             if (selectIndex + 1 < categoryListAdapter.data.size) {
                 data[selectIndex + 1].fillet = "up"
+                notifyItemRangeChanged(selectIndex, 2)
             }
-            notifyDataSetChanged()
         }
     }
 
     private fun scrollToMiddle(position: Int) {
-        val displayHeight = categoryList.height
-        categoryLayoutManager.scrollToPositionWithOffset(position, displayHeight / 2 - 48)
+        var firstPosition = categoryLayoutManager.findFirstVisibleItemPosition()
+        //当前点击的item距离 recyclerview 顶部的距离
+        var top = categoryList.getChildAt(position - firstPosition).top
+        //recyclerView可视区域高度-当前点击item的高度 的一半高度
+        var half = visualHeight/2
+
+        categoryList.smoothScrollBy(0, top - half)
     }
 }
--- module_category/src/main/java/com/aries/category/ui/view/CategoryRightView.kt
+++ module_category/src/main/java/com/aries/category/ui/view/CategoryRightView.kt
@@ -8,18 +8,18 @@ import android.view.MotionEvent
 import android.widget.FrameLayout
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import coil.Coil
-import coil.ImageLoader
 import coil.load
 import com.aries.category.ui.ContentCateResponse
 import com.aries.category.R
 import com.aries.category.ui.adapter.SectionQuickAdapter
 import com.aries.category.ui.modal.CategoryModal
+import com.aries.common.util.CoilUtil
 import com.google.android.material.tabs.TabLayout
+import kotlinx.android.synthetic.main.fragment_main.*
 import kotlinx.android.synthetic.main.main_right.view.*
 
 class CategoryRightView: FrameLayout {
-    private lateinit var imageLoader: ImageLoader
+    private val imageLoader = CoilUtil.getImageLoader()
     private lateinit var sectionQuickAdapter: SectionQuickAdapter
     private lateinit var gridLayoutManager: GridLayoutManager
     private lateinit var dataCopy: ContentCateResponse
@@ -28,6 +28,9 @@ class CategoryRightView: FrameLayout {
     private var lastPos: Int = -1
     private var isRecyclerScroll: Boolean = false
 
+    private var mToPosition: Int = 0 //记录目标位置
+    private var mShouldScroll: Boolean = false //目标项是否在最后一个可见项之后
+
     constructor(context: Context) : this(context, null)
     constructor(context: Context, attributeSet: AttributeSet?) : this(context, attributeSet, 0)
     constructor(context: Context, attributeSet: AttributeSet?, defStyleAttr: Int) : super(
@@ -35,17 +38,17 @@ class CategoryRightView: FrameLayout {
         attributeSet,
         defStyleAttr
     ) {
-        init(context)
+        initView(context)
     }
 
     @SuppressLint("ClickableViewAccessibility")
-    private fun init(context: Context) {
-        imageLoader = Coil.imageLoader(context)
-        gridLayoutManager = GridLayoutManager(context, 3)
+    private fun initView(context: Context) {
         LayoutInflater.from(context).inflate(R.layout.main_right, this, true)
 
+        gridLayoutManager = GridLayoutManager(context, 3)
         sectionQuickAdapter = SectionQuickAdapter(R.layout.main_right_grid_header, R.layout.main_right_grid, arrayListOf())
-        var _tabLayout = tabLayout
+
+        val thisTabLayout = tabLayout
         categoryRecyclerView.run {
             layoutManager = gridLayoutManager
             adapter = sectionQuickAdapter
@@ -60,11 +63,18 @@ class CategoryRightView: FrameLayout {
                 override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
                     super.onScrolled(recyclerView, dx, dy)
                     if (isRecyclerScroll) {
-                        var position = findCateIndex(gridLayoutManager.findFirstVisibleItemPosition())
+                        val position = findHeaderPositionByTab(gridLayoutManager.findFirstVisibleItemPosition())
                         if (position != lastPos) {
-                            _tabLayout.setScrollPosition(position, 0F, true)
+                            thisTabLayout.setScrollPosition(position, 0F, true)
                         }
                         lastPos = position
+                    }
+                }
+                override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                    super.onScrollStateChanged(recyclerView, newState)
+                    if (mShouldScroll) {
+                        mShouldScroll = false
+                        scrollTop(mToPosition)
                     }
                 }
             })
@@ -72,15 +82,13 @@ class CategoryRightView: FrameLayout {
         tabLayout.run {
             addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
                 override fun onTabSelected(tab: TabLayout.Tab?) {
-                    var position = tab?.position
+                    val position = tab?.position
                     if (position != null) {
-                        scrollCycleViewByTabPosition(position)
+                        scrollRecycleView2Top(position)
                     }
                     isRecyclerScroll = false
                 }
-
                 override fun onTabUnselected(tab: TabLayout.Tab?) {}
-
                 override fun onTabReselected(tab: TabLayout.Tab?) {}
             })
         }
@@ -94,43 +102,61 @@ class CategoryRightView: FrameLayout {
         tabLayout.removeAllTabs()
         data.cateList.forEach { v -> tabLayout.addTab(tabLayout.newTab().setText(v.categoryName)) }
 
-        var list: ArrayList<CategoryModal> = arrayListOf()
+        val list: ArrayList<CategoryModal> = arrayListOf()
         data.cateList.forEach { v ->
             run {
-                list.add(CategoryModal(v?.iconUrl, v.categoryName, v.categoryCode, true))
-                v.cateList?.forEach { m ->
-                    run {
-                        list.add(CategoryModal(m?.iconUrl, m.categoryName, m.categoryCode, false))
-                    }
-                }
+                list.add(CategoryModal(v.iconUrl, v.categoryName, v.categoryCode, true))
+                v.cateList?.forEach { m -> list.add(CategoryModal(m.iconUrl, m.categoryName, m.categoryCode, false))}
             }
         }
         sectionQuickAdapter.setList(list)
     }
 
-    private fun findCateIndex(position: Int): Int {
-        var currentItem = sectionQuickAdapter.data[position]
-        var index = dataCopy.cateList.indexOfFirst { group -> group.categoryCode == currentItem.categoryCode }
+
+    private fun scrollRecycleView2Top(position: Int) {
+        val currentItem = dataCopy.cateList[position]
+        val currentPosition = sectionQuickAdapter.data.indexOfFirst { v -> v.categoryCode == currentItem.categoryCode }
+
+        scrollTop(currentPosition)
+    }
+
+    private fun scrollTop(position: Int) {
+        val firstPosition = gridLayoutManager.findFirstVisibleItemPosition()
+        val lastPosition = gridLayoutManager.findLastVisibleItemPosition()
+
+        if (position < firstPosition) {
+            // 如果跳转位置在第一个可见位置之前，就smoothScrollToPosition可以直接跳转
+            categoryRecyclerView.smoothScrollToPosition(position)
+        } else if (position <= lastPosition){
+            // 跳转位置在第一个可见项之后，最后一个可见项之前
+            // smoothScrollToPosition根本不会动，此时调用smoothScrollBy来滑动到指定位置
+            val movePosition = position - firstPosition
+            if (movePosition >=0 && movePosition < categoryRecyclerView.childCount) {
+                val scrollY = categoryRecyclerView.getChildAt(position - firstPosition).top
+                categoryRecyclerView.smoothScrollBy(0, scrollY)
+            }
+        } else {
+            // 如果要跳转的位置在最后可见项之后，则先调用smoothScrollToPosition将要跳转的位置滚动到可见位置
+            // 再通过onScrollStateChanged控制再次调用smoothMoveToPosition，执行上一个判断中的方法
+            categoryRecyclerView.smoothScrollToPosition(position)
+            mToPosition = position
+            mShouldScroll = true
+        }
+    }
+
+    private fun findHeaderPositionByTab(position: Int): Int {
+        val currentItem = sectionQuickAdapter.data[position]
+        val index = dataCopy.cateList.indexOfFirst { group -> group.categoryCode == currentItem.categoryCode }
 
         if(index != -1){
             return index
         } else {
             var innerIndex: Int
-            return dataCopy.cateList.indexOfFirst { group ->
-                innerIndex =
-                    group.cateList?.indexOfFirst { interGroup -> interGroup.categoryCode == currentItem.categoryCode }!!
+            return dataCopy.cateList.indexOfFirst {
+                    group -> innerIndex = group.cateList?.indexOfFirst { interGroup -> interGroup.categoryCode == currentItem.categoryCode }!!
 
                 return@indexOfFirst innerIndex != -1
             }
-        }
-    }
-
-    private fun scrollCycleViewByTabPosition(position: Int) {
-        if (sectionQuickAdapter.data.size > 0) {
-            var parent = dataCopy.cateList[position]
-            var index = sectionQuickAdapter.data.indexOfFirst { v -> v.categoryCode == parent.categoryCode }
-
-            gridLayoutManager.scrollToPositionWithOffset(index, 0)
         }
     }
 }
