2e4e81fbf22f0742d61d203cb183651012912f48
刘辉20020013 <20020013@suning.com>
2022-03-19 18:14:54 +0800

    feat: 购物车的页面布局

--- module_cart/src/main/java/com/aries/cart/ui/CartFragment.kt
+++ module_cart/src/main/java/com/aries/cart/ui/CartFragment.kt
@@ -1,7 +1,6 @@
 package com.aries.cart.ui
 
 import android.view.View
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.airbnb.mvrx.MavericksView
 import com.airbnb.mvrx.activityViewModel
@@ -11,24 +10,23 @@ import com.aries.cart.R
 import com.aries.cart.ui.view.QuickEntryPopup
 import com.aries.common.adapter.GoodsListAdapter
 import com.aries.common.decoration.SpacesItemDecoration
-import com.aries.common.util.DisplayUtil
 import com.aries.common.util.PixelUtil
 import com.aries.common.util.StatusBarUtil
 import com.aries.common.util.UnreadMsgUtil
 import com.google.android.material.appbar.AppBarLayout
 import com.lxj.xpopup.XPopup
 import com.lxj.xpopup.enums.PopupAnimation
-import kotlinx.android.synthetic.main.bottom_all_select.*
 import kotlinx.android.synthetic.main.fragment_cart.*
 import kotlinx.android.synthetic.main.top_address.*
 
 class CartFragment : BaseFragment(R.layout.fragment_cart), MavericksView {
     private val viewModel: CartViewModel by activityViewModel()
-    private val maybeLikeListAdapter: GoodsListAdapter by lazy { GoodsListAdapter(arrayListOf())}
+    private val topViewHeight = StatusBarUtil.getHeight() + PixelUtil.toPixelFromDIP(40f).toInt()
+
+    private val goodsListAdapter by lazy { GoodsListAdapter(arrayListOf()) }
     private val staggeredGridLayoutManager: StaggeredGridLayoutManager by lazy {
         StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
     }
-    private val topViewHeight = StatusBarUtil.getHeight() + PixelUtil.toPixelFromDIP(40f).toInt()
 
     override fun initView() {
         initStatusBarPlaceholder()
@@ -54,12 +52,10 @@ class CartFragment : BaseFragment(R.layout.fragment_cart), MavericksView {
             setEnableLoadMore(false)
         }
 
-        //可能喜欢列表或者快来看看列表
-        staggeredGridLayoutManager.gapStrategy = StaggeredGridLayoutManager.GAP_HANDLING_NONE //解决加载下一页后重新排列的问题
-        maybeLikeListRecyclerView.run {
-            adapter = maybeLikeListAdapter
-            layoutManager = staggeredGridLayoutManager
+        goodsList.run {
             addItemDecoration(SpacesItemDecoration(10))
+            layoutManager = staggeredGridLayoutManager
+            adapter = goodsListAdapter
         }
     }
 
@@ -90,7 +86,7 @@ class CartFragment : BaseFragment(R.layout.fragment_cart), MavericksView {
     override fun invalidate() {
         withState(viewModel) {
             if (it.goodsList.isNotEmpty()) {
-                maybeLikeListAdapter.setList(it.goodsList)
+                goodsListAdapter.setList(it.goodsList)
             }
         }
     }
