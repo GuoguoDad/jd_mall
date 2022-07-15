f88ff52f237619deadb5b8d3fab3bf9d46875466
刘辉20020013 <20020013@suning.com>
2022-07-15 09:56:23 +0800

    feat: 开屏页跳转工作台转场动画

--- module_main/src/main/java/com/aries/main/ui/splash/StartPageActivity.kt
+++ module_main/src/main/java/com/aries/main/ui/splash/StartPageActivity.kt
@@ -2,7 +2,9 @@ package com.aries.main.ui.splash
 
 import android.app.Activity
 import android.os.Bundle
+import com.alibaba.android.arouter.facade.Postcard
 import com.alibaba.android.arouter.facade.annotation.Route
+import com.alibaba.android.arouter.facade.callback.NavigationCallback
 import com.alibaba.android.arouter.launcher.ARouter
 import com.aries.common.constants.RouterPaths
 import com.aries.main.R
@@ -13,10 +15,19 @@ class StartPageActivity: Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         ImmersionBar.with(this).transparentBar().init()
+
         Thread{
             Thread.sleep(1000)
-            ARouter.getInstance().build(RouterPaths.MAIN_ACTIVITY).navigation()
-            finish()
+            ARouter.getInstance().build(RouterPaths.MAIN_ACTIVITY)
+                .withTransition(0, R.anim.fade_out)
+                .navigation(this, object : NavigationCallback {
+                    override fun onFound(postcard: Postcard?) {}
+                    override fun onLost(postcard: Postcard?) {}
+                    override fun onArrival(postcard: Postcard?) {
+                        finish()
+                    }
+                    override fun onInterrupt(postcard: Postcard?) {}
+                })
         }.start()
     }
 }
