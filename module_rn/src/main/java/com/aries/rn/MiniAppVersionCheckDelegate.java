cb4930d517a4c39ae3c62879ae51ad69b9c3461b
刘辉20020013 <20020013@suning.com>
2022-09-28 14:51:43 +0800

    feat: crash need to fix

--- module_rn/src/main/java/com/aries/rn/MiniAppVersionCheckDelegate.java
+++ module_rn/src/main/java/com/aries/rn/MiniAppVersionCheckDelegate.java
@@ -38,8 +38,8 @@ public class MiniAppVersionCheckDelegate implements VersionCheckerDelegate {
     @Override
     public void processCheckResult(Map<String, Object> params, Callback callback) {
         Request request = new Request.Builder()
-                .url("https://oss.suning.com/sffe/sffe/aries/index.json")
-//                .url("https://oss.suning.com/sffe/sffe/singlePage/index.json")
+//                .url("https://oss.suning.com/sffe/sffe/aries/index.json")
+                .url("https://oss.suning.com/sffe/sffe/70/index.json")
                 .build();
 
         client.newCall(request).enqueue(new okhttp3.Callback() {
