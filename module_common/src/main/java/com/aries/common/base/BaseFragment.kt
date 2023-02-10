5cae24012f66a6143dff0e38aa5ca42603f8b99f
刘辉20020013 <20020013@suning.com>
2023-02-10 15:45:05 +0800

    feat: 改造BaseActivity、BaseFragment， ViewBinding方式替代插件kotlin-android-extensions

--- module_common/src/main/java/com/aries/common/base/BaseFragment.kt
+++ module_common/src/main/java/com/aries/common/base/BaseFragment.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
 
